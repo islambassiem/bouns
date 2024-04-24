af38f631986eecb33e123a89cdf52b67f6c0873e
Islam <islambassiem@gmail.com>
2024-04-24 08:51:31 +0300

    edit audio file

--- public/script.js
+++ public/script.js
@@ -11,7 +11,6 @@ let countdownTimer = new Audio('countdown.m4a');
 
 circleContainer.addEventListener('click', () =>{
     let duration = 10;
-    countdownTimer.currentTime = 3;
     countdownTimer.play();
     const interval = setInterval(() => {
         countdown.innerHTML = `<span>${duration}</span>`;
